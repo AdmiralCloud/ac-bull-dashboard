48a0a2a37bd25150e80e683605ce06cf15f0daf4
Julius Eckert <eckert.julius@gmail.com>
2026-02-21 18:27:48 +0100

    AC-8314 Default Refresh Rate = 30 sec

--- src/context/OptionsContextProvider.tsx
+++ src/context/OptionsContextProvider.tsx
@@ -9,13 +9,13 @@ export interface Props {
 export const OptionsContext = React.createContext( {
     env: autoDecideEnv() as ( 'dev' | 'live' ),
     setEnv: ( () => null ) as React.Dispatch<React.SetStateAction<string>>,
-    refreshInterval: 3000 as number,
+    refreshInterval: 30000 as number,
     setRefreshInterval: ( () => null ) as React.Dispatch<React.SetStateAction<number>>,
 } )
 
 const OptionsContextProvider: React.FC<Props> = ( { children } ) => {
     const [ env, setEnv ] = useState( autoDecideEnv() as 'dev' | 'live' )
-    const [ refreshInterval, setRefreshInterval ] = useState( 3000 )
+    const [ refreshInterval, setRefreshInterval ] = useState( 30000 )
 
     return (
         <OptionsContext.Provider value={ {
